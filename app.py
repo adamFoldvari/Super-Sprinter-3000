78c7176afad8bf77ce2e90cb9cdc6e78f22fb5cc
Ádám Földvari <foldadam93@gmail.com>
2017-05-08 16:19:49 +0200

    exporting data now use csv module

--- app.py
+++ app.py
@@ -5,27 +5,25 @@ import os
 app = Flask(__name__)
 
 
-def read_data_from_csv(filename):
+def read_data_from_csv():
     story_list = []
-    with open(os.path.join(os.path.dirname(__file__), filename), 'r') as workfile:
+    with open('story_list.csv', 'r') as workfile:
         reader = csv.reader(workfile)
         for row in reader:
             story_list.append(row[0].split(';'))
     return story_list
 
 
-def add_data_to_csv(filename, story_list):
-    with open(os.path.join(os.path.dirname(__file__), filename), 'w') as workfile:
-        for item in story_list:
-            story = [element.replace("\r\n", " ") for element in item]
-            row = ';'.join(story)
-            workfile.write(row + "\n")
+def add_data_to_csv(story_list):
+    with open('story_list.csv', 'w') as workfile:
+        datawriter = csv.writer(workfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        datawriter.writerows(story_list)
 
 
 @app.route('/')
 @app.route('/list')
 def list_page():
-    readed_story_list = read_data_from_csv('story_list.csv')
+    readed_story_list = read_data_from_csv()
     return render_template("list.html", story_list=readed_story_list)
 
 
@@ -37,7 +35,7 @@ def story_page(story_id=None):
             empyt_list.append(' ')
         return render_template('form.html', story_id=story_id, data_to_edit=empyt_list)
     else:
-        story_list = read_data_from_csv('story_list.csv')
+        story_list = read_data_from_csv()
         new_row = [str(len(story_list)+1)]
         form_elements = [
                         "story_title",
@@ -50,13 +48,13 @@ def story_page(story_id=None):
         for element in form_elements:
             new_row.append(request.form[element])
         story_list.append(new_row)
-        add_data_to_csv('story_list.csv', story_list)
+        add_data_to_csv(story_list)
         return redirect(url_for('list_page'))
 
 
 @app.route('/story/<story_id>')
 def update_strory(story_id):
-    story_list = read_data_from_csv('story_list.csv')
+    story_list = read_data_from_csv()
     for row in story_list:
         if row[0] == story_id:
             editable_row = row
@@ -65,7 +63,7 @@ def update_strory(story_id):
 
 @app.route('/story/update/<story_id>', methods=['POST'])
 def update_data(story_id):
-    story_list = read_data_from_csv('story_list.csv')
+    story_list = read_data_from_csv()
     form_elements = [
                     "story_title",
                     "user_story",
@@ -78,17 +76,17 @@ def update_data(story_id):
         if story_id == story_list[index][0]:
             for num, element in enumerate(form_elements):
                 story_list[index][num+1] = request.form[element]
-    add_data_to_csv('story_list.csv', story_list)
+    add_data_to_csv(story_list)
     return redirect(url_for('list_page'))
 
 
 @app.route('/story/delete/<story_id>')
 def delete_story(story_id):
-    story_list = read_data_from_csv('story_list.csv')
+    story_list = read_data_from_csv()
     for row in story_list:
         if row[0] == story_id:
             story_list.remove(row)
-    add_data_to_csv('story_list.csv', story_list)
+    add_data_to_csv(story_list)
     return redirect(url_for('list_page'))
 
 
